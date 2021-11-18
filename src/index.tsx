313d0014124655346a6675039afaf435d220751b
onirenaud <renaud.rohlinger@gmail.com>
2021-11-18 13:26:54 +0900

    handle context

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,28 @@ import { useRef, useEffect } from 'react'
 import EventEmitter from 'eventemitter3'
 
 interface optionsType {
-  once?: boolean
+  once?: boolean,
+  context?: any
 }
 
 const eventEmitter = new EventEmitter()
 
 // vanilla
 const onEvent = (eventName: string, handler: any, options: optionsType = {}) => {
+  let context:any = null
   const { once } = options
   if (once) {
-    eventEmitter.once(eventName, handler)
+    context = eventEmitter.once(eventName, handler)
   } else {
-    eventEmitter.on(eventName, handler)
+    context = eventEmitter.on(eventName, handler)
   }
+  return context
 }
 
 // vanilla
 const offEvent = (eventName: string, handler: any, options: optionsType = {}) => {
-  const { once } = options
-  eventEmitter.removeListener(eventName, handler, null, once ? true : false)
+  const { once, context } = options
+  eventEmitter.removeListener(eventName, handler, context || null, once ? true : false)
 }
 
 // react hook
@@ -33,15 +36,15 @@ const useEvent = (eventName: string, handler: any, options: optionsType = {}) =>
   }, [handler])
 
   useEffect(() => {
-
+    let context:any = null
     const eventListener = (event:any) => savedHandler.current(event)
     if (once) {
-      eventEmitter.once(eventName, eventListener)
+      context = eventEmitter.once(eventName, eventListener)
     } else {
-      eventEmitter.on(eventName, eventListener)
+      context = eventEmitter.on(eventName, eventListener)
     }
     return () => {
-      eventEmitter.removeListener(eventName, eventListener, null, once ? true : false)
+      eventEmitter.removeListener(eventName, eventListener, context, once ? true : false)
     }
   }, [eventName, once])
 }
